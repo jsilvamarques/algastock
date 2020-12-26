08ec0040fb66b1b317cfa6ed2679720b8dbf90b3
Jefferson Marques da Silva <jefferson_marques17@hotmail.com>
2020-12-26 10:58:26 -0300

    ciclos de vida de um componente

--- src/components/ClassComponent/ClassComponent.tsx
+++ src/components/ClassComponent/ClassComponent.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
 
 class ClassComponent extends React.Component<{ name: String }> {  
+
+    constructor(props: any){
+        super(props)
+        console.log('contructor reached')
+    }
+
     state = {
         name: 'Mundo!!!!!'
     }
 
+    componentDidMount() {
+        console.log('componentDidMount reached')
+    }
+
+    componentDidUpdate() {
+        console.log('componentDidUpdate reached')
+    }
+
     render() {
+      console.log('render reached')
       return <div>
           Olá, {this.state.name}, eu sou um component baseado em class
             <br />
